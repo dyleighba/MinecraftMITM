0e9b0b2bcb9814c5bc2f57c9b67a7a7376198d6f
Dylan Barber <dylan@dlbarber.dev>
2022-12-01 10:44:29 +0800

    Moved tests for Position struct

--- test/type/test_Position.cpp
+++ test/type/test_Position.cpp
@@ -1,5 +1,5 @@
 //
-// Created by Dylan on 11/28/2022.
+// Created by Dylan on 12/1/2022.
 //
 #include "catch2/generators/catch_generators.hpp"
 #include <catch2/catch_test_macros.hpp>
@@ -10,16 +10,13 @@ bool isAproxEqual(double d1, double d2) {
     return abs(d1 - d2) < 0.1;
 }
 
-bool testPosition(Position testPos) {
+bool testPositionClass(Position testPos) {
     std::vector<unsigned char> generatedBytes = testPos.bytes();
     if (generatedBytes.size() != 25) {
         printf("Incorrect packet size: %zu", generatedBytes.size());
         return false;
     }
     Position readbackPos = Position::parse(generatedBytes);
-    printf("in: (%.2f, %.2f, %.2f, %s),  out: (%.2f, %.2f, %.2f, %s)",
-           testPos.x, testPos.y, testPos.z, testPos.onGround ? "True" : "False",
-           readbackPos.x, readbackPos.y, readbackPos.z, readbackPos.onGround ? "True" : "False");
     if (!isAproxEqual(testPos.x, readbackPos.x)) return false;
     if (!isAproxEqual(testPos.y, readbackPos.y)) return false;
     if (!isAproxEqual(testPos.z, readbackPos.z)) return false;
@@ -30,7 +27,7 @@ TEST_CASE( "Position packet creation") {
     double y = GENERATE(-20.5, 87, 65.5, -40);
     double z = GENERATE(-4968.4, 853.1, -7.1, 5.3);
     bool onGround = GENERATE(true, false);
-    REQUIRE(testPosition(Position {
-        x, y, z, onGround
+    REQUIRE(testPositionClass(Position {
+            x, y, z, onGround
     }));
 }
